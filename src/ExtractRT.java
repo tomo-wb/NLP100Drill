1e5c755e7f5d922ddcf9fbc1c270ff3555bad2a1
Tomoya Mizumoto <tm0mo.3120@gmail.com>
2014-05-05 12:34:31 +0900

    add ExtractRT

--- src/ExtractRT.java
+++ src/ExtractRT.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 public class ExtractRT {
     private static String RT = "RT";
     public static void main(String[] args){
+        /*
         FileIO FIO = new FileIO();
         ArrayList<String> TextsArray = new ArrayList<>();  // stored texts
         TextsArray = FIO.FileSTDIN();
@@ -22,7 +23,43 @@ public class ExtractRT {
                 println("O "+TextsArray.get(i));
                 println("E "+ RTreply);
             }
-        }
+        }*/
+        String text = "RT @??: すうのよ RT @???: つい RT @???: なのだ";
+        recursion(text);
     }
+    
+    private static void recursion(String text){
+        Pattern p = Pattern.compile("(.*) "+RT+" @.*: (.*)");
+        Matcher m = p.matcher(text);
+        if(m.find()){
+            String RTreply = m.group(1);
+            m = p.matcher(RTreply);
+            if(m.find()){
+                String RTreply2 = m.group(2);
+                println(RTreply2);
+                recursion(RTreply);
+            }
+            else{
+                Pattern p2 = Pattern.compile(RT+" @.*: (.*)");
+                println(RTreply);
+                Matcher m2 = p2.matcher(RTreply);
+                if(m2.find()){
+                    String RTreply2 = m2.group(1);
+                    println(RTreply2);
+                }
+                else{
+                
+                }
+                //Matcher m2 = p2.matcher(RTreply);
+                //String RTreply2 = m2.group(1);
+                //println(RTreply2);
+            }
+        }
+        else{
+            println(text);
+        }
+        //return text;
+    }
+    
     private static void println(Object obj) { System.out.println(obj); }
 }
