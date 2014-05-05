72b4c5bdfc6a47725ea4e1d7a65b23cddd0a3f28
Tomoya Mizumoto <tm0mo.3120@gmail.com>
2014-05-06 00:26:03 +0900

    add ExtrcatUser

---
+++ src/ExtractUser.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TomoyaMizumoto
+ */
+public class ExtractUser {
+    private static HashMap<String, Integer> User = new HashMap<>();
+    public static void main(String[] args){
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = br.readLine();
+            while(str != null){
+                int num = 0;
+                ExtractUser(str);
+                str = br.readLine();
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }       
+    }
+    
+    private static void ExtractUser(String str){
+        Pattern p = Pattern.compile("@[A-Za-z0-9_]+");
+        Matcher m = p.matcher(str);
+        while(m.find()){
+            String user = m.group();
+            if(!User.containsKey(user)){
+                println(user);
+                User.put(user, 1);
+            } 
+        }
+    }
+    private static void println(Object obj) { System.out.println(obj); }
+}
